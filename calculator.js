139b36740ed7109e21371b941fc4ef691549dea7
SilverPsalm <171736682+silverpsalm@users.noreply.github.com>
2025-05-28 10:14:45 -0400

    Move Equal and Clear button out of operand div. Add if else statement to number buttons and store their values to number variables.

--- calculator.js
+++ calculator.js
@@ -5,13 +5,14 @@ let multiply = (a,b) => a * b;
 let divide = (a,b) => a / b;
 
 
-let num1;
+let num1 = "";
 let operand;
-let num2;
+let num2 = "";
 
 const userOutput = document.querySelector("#output");
 const numBtns = document.querySelectorAll("div#numbers > button");
 
+const operands = document.querySelectorAll("div#operands > button");
 const addBtn = document.querySelector("#add");
 const subtractBtn = document.querySelector("#subtract");
 const multiplyBtn = document.querySelector("#multiply");
@@ -19,12 +20,28 @@ const divideBtn = document.querySelector("#divide");
 const equalBtn = document.querySelector("#equals");
 const clearBtn = document.querySelector("#clear");
 
+operands.forEach( opBtn => {
+
+    opBtn.addEventListener("click", () => {
+        userOutput.textContent += opBtn.textContent;
+        operand = opBtn.textContent;
+        console.log(operand);
+    });
+});
+
+// print and store operands only. Equals will perform operation.
 
 numBtns.forEach( button => {
 
     button.addEventListener("click", () => {
         userOutput.textContent += button.textContent;
+        if(!operand) {
+            num1 += button.textContent;
+            console.log(`button added to num1: ${num1}`);
+        } else {
+            num2 += button.textContent;
+            console.log(`button added to num2: ${num2}`);
+        }
     });
-
 });
 
